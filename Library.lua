9f25c0451b5932afa99f8fed68df6cf2febf5843
Synth <72616658+synthofficial@users.noreply.github.com>
2023-01-29 20:59:54 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,6 +1,7 @@
 local WindowTable = {}
 
 function WindowTable:CreateWindow()
+
 	-- Gui to Lua
 	-- Version: 3.2
 
@@ -12,22 +13,24 @@ function WindowTable:CreateWindow()
 	local HubName = Instance.new("TextLabel")
 	local allPages = Instance.new("Frame")
 	local pageCorner = Instance.new("UICorner")
+	local pagesFolder = Instance.new("Folder")
+	local newPage = Instance.new("ScrollingFrame")
+
 	local mainCorner = Instance.new("UICorner")
 	local SideBar = Instance.new("Frame")
 	local sideCorner = Instance.new("UICorner")
-	local allTabs = Instance.new("ScrollingFrame")
-	local tabListing = Instance.new("UIListLayout")
+
 
 	--Properties:
 
 	Main.Name = "Main"
-	Main.Parent = game.CoreGui
+	Main.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 	Main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 	MainFrame.Name = "MainFrame"
 	MainFrame.Parent = Main
 	MainFrame.BackgroundColor3 = Color3.fromRGB(56, 56, 56)
-	MainFrame.Position = UDim2.new(0.261538476, 0, 0.207317069, 0)
+	MainFrame.Position = UDim2.new(0.287230164, 0, 0.311678439, 0)
 	MainFrame.Size = UDim2.new(0, 583, 0, 372)
 
 	TopBar.Name = "TopBar"
@@ -58,6 +61,19 @@ function WindowTable:CreateWindow()
 	pageCorner.Name = "pageCorner"
 	pageCorner.Parent = allPages
 
+	pagesFolder.Name = "pagesFolder"
+	pagesFolder.Parent = allPages
+
+	newPage.Name = "newPage"
+	newPage.Parent = allPages
+	newPage.Active = true
+	newPage.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
+	newPage.BorderSizePixel = 0
+	newPage.Position = UDim2.new(0, 0, 4.70950248e-08, 0)
+	newPage.Size = UDim2.new(0, 442, 0, 324)
+	newPage.ScrollBarThickness = 4
+
+
 
 	mainCorner.CornerRadius = UDim.new(0, 4)
 	mainCorner.Name = "mainCorner"
@@ -73,53 +89,29 @@ function WindowTable:CreateWindow()
 	sideCorner.Name = "sideCorner"
 	sideCorner.Parent = SideBar
 
-	allTabs.Name = "allTabs"
-	allTabs.Parent = SideBar
-	allTabs.Active = true
-	allTabs.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	allTabs.BackgroundTransparency = 1.000
-	allTabs.BorderSizePixel = 0
-	allTabs.Size = UDim2.new(0, 118, 0, 324)
-	allTabs.ScrollBarThickness = 4
-
-	tabListing.Name = "tabListing"
-	tabListing.Parent = allTabs
-
-	tabListing.SortOrder = Enum.SortOrder.LayoutOrder
-	tabListing.Padding = UDim.new(0, 5)
-	
-	local pagesFolder = Instance.new("Folder")
-
-	pagesFolder.Name = "pagesFolder"
-	pagesFolder.Parent = allPages
-
-
 	
 	local TabHandler = {}
 	
 	function TabHandler:CreateTab(tabname)
 		tabname = tabname or "New Tab"
 		
-		local TabButton = Instance.new("TextButton")
-		local newPage = Instance.new("ScrollingFrame")
-		local elementsListing = Instance.new("UIListLayout")
+		local TabButton = Instance.new("TextLabel")
+		local tabListing = Instance.new("UIListLayout")
 		local tabPadding = Instance.new("UIPadding")
-
-		--Properties:
-
-
+		local elementsListing = Instance.new("UIListLayout")
 		
+
 		TabButton.Name = "TabButton"
-		TabButton.Parent = allTabs
+		TabButton.Parent = SideBar
 		TabButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		TabButton.BackgroundTransparency = 1.000
+		TabButton.Position = UDim2.new(0.0851947516, 0, 0.0309606474, 0)
 		TabButton.Size = UDim2.new(0, 100, 0, 28)
 		TabButton.Font = Enum.Font.FredokaOne
-		TabButton.Text = tabname
-		TabButton.TextColor3 = Color3.fromRGB(125,125,125) --green 189, 255, 161
+		TabButton.Text = "New Tab"
+		TabButton.TextColor3 = Color3.fromRGB(189, 255, 161)
 		TabButton.TextSize = 14.000
-		TabButton.TextXAlignment = Enum.TextXAlignment.Center
-		TabButton.TextYAlignment = Enum.TextYAlignment.Center
+
 
 		TabButton.MouseButton1Click:Connect(function()
 			for i,v in next, pagesFolder:GetChildren() do -- We get all the pages that we added
@@ -127,7 +119,7 @@ function WindowTable:CreateWindow()
 			end 
 			newPage.Visible = true  -- We make current page visible but not others
 			
-			for i,v in next, allTabs:GetChildren() do   -- We get all the elements inside the frame
+			for i,v in next, SideBar:GetChildren() do   -- We get all the elements inside the frame
 				if v:IsA("TextButton") then -- We can't animate UIListLayout, so we check if its a TextButton
 					game.TweenService:Create(v, TweenInfo.new(0.2, Enum.EasingStyle.Linear, Enum.EasingDirection.In), {
 						TextColor3 = Color3.fromRGB(125,125,125) -- We animate other Tab Buttons and making the current one seem Checked
@@ -140,25 +132,20 @@ function WindowTable:CreateWindow()
 
 		end)
 		
-		newPage.Name = "newPage"
-		newPage.Parent = pagesFolder
-		newPage.Active = true
-		newPage.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
-		newPage.BorderSizePixel = 0
-		newPage.Size = UDim2.new(0, 442, 0, 324)
-		newPage.ScrollBarThickness = 4
 
-		elementsListing.Name = "elementsListing"
-		elementsListing.Parent = newPage
-		elementsListing.HorizontalAlignment = Enum.HorizontalAlignment.Center
-		elementsListing.SortOrder = Enum.SortOrder.LayoutOrder
-		elementsListing.Padding = UDim.new(0, 2)
-		
+		tabListing.Name = "tabListing"
+		tabListing.Parent = SideBar
+		tabListing.SortOrder = Enum.SortOrder.LayoutOrder
+		tabListing.Padding = UDim.new(0, 2)
 
 		tabPadding.Name = "tabPadding"
-		tabPadding.Parent = game.StarterGui.Main.MainFrame.SideBar
+		tabPadding.Parent = SideBar
 		tabPadding.PaddingLeft = UDim.new(0, 8)
 		
+		elementsListing.Name = "elementsListing"
+		elementsListing.Parent = newPage
+		elementsListing.SortOrder = Enum.SortOrder.LayoutOrder
+		elementsListing.Padding = UDim.new(0, 5)
 		
 	end
 	
